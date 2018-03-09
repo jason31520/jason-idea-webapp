7228c08ccd0d1c6dff706bcc03baa5eeff33a67e
Jason315 <lyna318@163.com>
2018-03-09 17:40:27 +0800

    modify something

--- www/handlers.py
+++ www/handlers.py
@@ -11,9 +11,13 @@ from models import User, Comment, Blog
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
-    print(users)
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
     return {
-        '__template__': 'index.html',
-        'users': users
-}
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
